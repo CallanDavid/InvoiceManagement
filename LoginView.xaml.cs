8f1dd52c95505b366f14e3366c0d71cf19922bc1
Callan Merifield <callan.merifield@gmail.com>
2025-04-23 15:13:02 +0200

    Add project files.

---
+++ LoginView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InvoiceManagement
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+
+
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        public void OnLoginBtnClicked(object sender, RoutedEventArgs e)
+        {
+            string passwordEntered = PasswordBox.Password;
+
+            string? envPw = Environment.GetEnvironmentVariable("InvoiceManagement");
+
+            if (envPw != null)
+            {
+                if (passwordEntered == envPw)
+                {
+                    MessageBox.Show("Correct Password Entered");
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Passwrod Entered");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Environment Variable not found...");
+            }
+        }
+        public void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            LoginButton.IsEnabled = !string.IsNullOrEmpty(PasswordBox.Password);
+        }
+    } 
+}
+
